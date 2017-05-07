7a75bbbd6736e5d1cc741b3ff781a18f24196db9
Ken Snyder <ken@ken.net>
2017-05-07 11:13:07 -0700

    Babel 6 and Ember 2.12 (problems with 2.13)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* jshint node: true */
+/* eslint-env node */
 'use strict';
 const path = require('path');
 
@@ -9,12 +9,11 @@ module.exports = {
     this._super.included(app);
     var app = appOrAddon.app || appOrAddon;
     if (!app.registry.availablePlugins['ember-cli-sass']) {
-      this.ui.writeLine(chalk.bold('ui-tag: ') + ' did not detect ' + chalk.bold.green('ember-cli-sass') + ', this package\'s styling depends on SASS.');
+      this.ui.writeLine('ui-tag: did not detect ember-cli-sass, this package\'s styling depends on SASS.');
     } else {
       const sassOptions = app.options.sassOptions || { includePaths: []};
       // app.import('ui-tag/styles/ui-tag.scss');
       sassOptions.includePaths.push(path.join(__dirname, 'ui-tabs'));
     }
-
   }
 };
